ba1cdef7e26021558a825fc89a3297b8c8949236
Trixster25 <79571699+Trixster25@users.noreply.github.com>
2025-03-14 21:29:32 +0800

    Add some lines

--- CLI_Appliaction.java
+++ CLI_Appliaction.java
@@ -3,5 +3,6 @@ public class CLI_Appliaction {
     
     public static void main(String[] args){
         System.out.println("Welcome to Trixster Enterprice");
+        System.out.println("1.Log-in\n2.Register\n");
     }
 }
